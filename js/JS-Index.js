3ec93ac1b27364a19ccebec579589012d53b2f03
Augusto248 <augustodibona@hotmail.com>
2020-10-01 20:12:09 -0300

    mejoraVisual

--- js/JS-Index.js
+++ js/JS-Index.js
@@ -1,7 +1,24 @@
-$(window).load(function()
+$(document).ready(function()
 {
   var div=$("#ajax");
   div.html("<p>test</p>");
+  //getElementsAjax();
   
 
 });
+
+
+function getElementsAjax()
+{
+    var jqxhr = $.get( "https://api.rawg.io/api/platforms", function() 
+    {
+      })
+        .done(function(data) {
+          alert( "second success" );
+          console.log(data.results[0].name);
+        })
+        .fail(function() {
+          alert( "error" );
+        });
+       
+}
